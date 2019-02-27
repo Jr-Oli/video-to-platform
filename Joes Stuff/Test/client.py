5d2405f4107d5c4675dd0cc40315a7ffaec746bd
Josephfrantz92 <35854050+Josephfrantz92@users.noreply.github.com>
2019-02-27 13:10:37 -0500

    Idk what was added. But it says things have changed.
    +Idk

--- Joes Stuff/Test/client.py
+++ Joes Stuff/Test/client.py
@@ -7,6 +7,7 @@ import time
 import sys
 
 
+
 #Host/Port
 HOST, PORT = "localhost", 9999
 
@@ -30,13 +31,17 @@ newZ0 = round(find_movement(data[0][5],data[1][5]),2)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Connect to server and send data
     sock.connect((HOST, PORT))
-    sock.sendall(bytes("Current platform position: " + str(data[0]) + "\n","utf_8"))
-    sock.sendall(bytes("Moving to position: " + str(data[1]) + "\n","utf_8"))
-    sock.sendall(bytes("Our platform needs to move it's X axis by " + str(newX) + " units. \n","utf_8"))
-    sock.sendall(bytes("Our platform needs to move it's Y axis by " + str(newY) + " units.\n","utf_8"))
-    sock.sendall(bytes("Our platform needs to move it's Z axis by " + str(newZ) + " units.\n","utf_8"))
-    sock.sendall(bytes("Our platform needs to move it's X0 axis by " + str(newX0) + " units.\n","utf_8"))
-    sock.sendall(bytes("Our platform needs to move it's Y0 axis by " + str(newY0) + " units.\n","utf_8"))
+    
+    #Uncomment for debugging purposes#
+    #sock.sendall(bytes("Current platform position: " + str(data[0]) + "\n","utf_8"))
+    #sock.sendall(bytes("Moving to position: " + str(data[1]) + "\n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's X axis by " + str(newX) + " units. \n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's Y axis by " + str(newY) + " units.\n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's Z axis by " + str(newZ) + " units.\n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's X0 axis by " + str(newX0) + " units.\n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's Y0 axis by " + str(newY0) + " units.\n","utf_8"))
+    #sock.sendall(bytes("Our platform needs to move it's Z0 axis by " + str(newZ0) + " units.\n","utf_8"))
 
-
+    for x in data[0]:
+        sock.sendall(bytes(str(x) +"\n","utf_8"))
 p.free_resources()
--- Joes Stuff/Test/server.py
+++ Joes Stuff/Test/server.py
@@ -11,9 +11,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(512).strip()
+        data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data.decode('utf_8'))
+        print(data.decode('utf_8'))
         
 
 if __name__ == "__main__":
